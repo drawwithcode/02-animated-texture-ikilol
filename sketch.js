4ba13b597ecf79ce17f472bfc17a5f2ae62baf76
ikilol <87668373+ikilol@users.noreply.github.com>
2021-10-11 13:08:24 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,98 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  colorMode(RGB);
+  noStroke();
+  background (230, 230, 230);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+
+  //Colore
+  var colorebase1 = 50;
+  var coloreok1 = colorebase1 + random()*100;
+
+  var colorebase2 = 50;
+  var coloreok2 = colorebase2 + random()*100;
+
+  fill (coloreok1, 0, coloreok2);
+
+
+ 
+  //Trigger mouse + Modifica dimensione
+  var spostamento1 = 100;
+  var spostamento2 = 200;
+  var spostamento3 = 300;
+
+  for(var x = 0; 
+    x < ((width/100)*100)+500; x+=spostamento1) {
+      for(var y = 0; 
+        y < ((height/100)*100)+500; y+=spostamento1){
+
+      var dimensione0 = 25;
+      var dimensione1 = 25;
+      var dimensione2 = 25;
+      var dimensione3 = 25;
+
+      if(mouseX > x - 80 
+        && mouseX < x + 20 
+        && mouseY > y - 80 
+        && mouseY < y + 20){
+
+        dimensione0 = dimensione0*4;
+        dimensione1 = dimensione1*3;
+        dimensione2 = dimensione2*2;
+        dimensione3 = dimensione3*1.5;
+      }
+
+
+      circle(x, y, dimensione0, dimensione0);
+
+      circle(x+spostamento1, y+spostamento1, dimensione1, dimensione1);
+      circle(x+spostamento1, y-spostamento1, dimensione1, dimensione1);
+      circle(x-spostamento1, y-spostamento1, dimensione1, dimensione1);
+      circle(x-spostamento1, y+spostamento1, dimensione1, dimensione1);
+
+      circle(x+spostamento1, y, dimensione1, dimensione1);
+      circle(x-spostamento1, y, dimensione1, dimensione1);
+      circle(x, y+spostamento1, dimensione1, dimensione1);
+      circle(x, y-spostamento1, dimensione1, dimensione1);
+
+      circle(x+spostamento2, y+spostamento2, dimensione2, dimensione2);
+      circle(x+spostamento2, y-spostamento2, dimensione2, dimensione2);
+      circle(x-spostamento2, y-spostamento2, dimensione2, dimensione2);
+      circle(x-spostamento2, y+spostamento2, dimensione2, dimensione2);
+
+      circle(x+spostamento2, y, dimensione2, dimensione2);
+      circle(x-spostamento2, y, dimensione2, dimensione2);
+      circle(x, y+spostamento2, dimensione2, dimensione2);
+      circle(x, y-spostamento2, dimensione2, dimensione2);
+
+      circle(x+spostamento1, y+spostamento2, dimensione2, dimensione2);
+      circle(x+spostamento1, y-spostamento2, dimensione2, dimensione2);
+      circle(x-spostamento2, y-spostamento1, dimensione2, dimensione2);
+      circle(x-spostamento2, y+spostamento1, dimensione2, dimensione2);
+
+      circle(x+spostamento2, y-spostamento1, dimensione2, dimensione2);
+      circle(x+spostamento2, y+spostamento1, dimensione2, dimensione2);
+      circle(x-spostamento1, y+spostamento2, dimensione2, dimensione2);
+      circle(x-spostamento1, y-spostamento2, dimensione2, dimensione2);
+
+      circle(x-spostamento2, y-spostamento3, dimensione3, dimensione3);
+      circle(x, y-spostamento3, dimensione3, dimensione3);
+      circle(x+spostamento2, y-spostamento3, dimensione3, dimensione3);
+
+      circle(x-spostamento3, y-spostamento2, dimensione3, dimensione3);
+      circle(x+spostamento3, y-spostamento2, dimensione3, dimensione3);
+
+      circle(x-spostamento3, y, dimensione3, dimensione3);
+      circle(x+spostamento3, y, dimensione3, dimensione3);
+
+      circle(x-spostamento3, y+spostamento2, dimensione3, dimensione3);
+      circle(x+spostamento3, y+spostamento2, dimensione3, dimensione3);
+
+      circle(x-spostamento2, y+spostamento3, dimensione3, dimensione3);
+      circle(x, y+spostamento3, dimensione3, dimensione3);
+      circle(x+spostamento2, y+spostamento3, dimensione3, dimensione3);
+    }
+  }
 }
